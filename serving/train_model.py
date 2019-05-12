ef1502db3b71fd3dbf40d231b15d4ae27bb7cb0f
mogoweb <mogoweb@gmail.com>
2019-05-12 22:15:15 +0800

    refine train model script

--- serving/train_model.py
+++ serving/train_model.py
@@ -10,16 +10,17 @@ import sys
 import absl
 import os
 
+
 FLAGS = None
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-IMG_SHAPE = (299, 299, 3)
+IMG_SHAPE = (192, 192, 3)
 BATCH_SIZE = 32
 
 
 def preprocess_image(image):
   image = tf.image.decode_jpeg(image, channels=3)
-  image = tf.image.resize(image, [299, 299])
+  image = tf.image.resize(image, [192, 192])
   image /= 255.0  # normalize to [0,1] range
   # normalized to the[-1, 1] range
   image = 2 * image - 1
@@ -33,13 +34,18 @@ def load_and_preprocess_image(path):
 
 
 def build_model(num_classes):
-  # Create the base model from the pre-trained model Inception V3
-  base_model = keras.applications.InceptionV3(input_shape=IMG_SHAPE,
-                                              # We cannot use the top classification layer of the pre-trained model as it contains 1000 classes.
-                                              # It also restricts our input dimensions to that which this model is trained on (default: 299x299)
-                                              include_top=False,
-                                              weights='imagenet')
-  base_model.trainable = False
+  # Create the base model from the pre-trained model MobileNet V2
+  base_model = keras.applications.MobileNetV2(input_shape=IMG_SHAPE, include_top=False)
+  # Unfreeze all layers of MobileNetV2
+  base_model.trainable = True
+
+  # Refreeze layers until the layers we want to fine-tune
+  for layer in base_model.layers[:100]:
+    layer.trainable = False
+
+  # Use a lower learning rate
+  lr = 0.0001
+
   # Using Sequential API to stack up the layers
   model = keras.Sequential([
     base_model,
@@ -49,7 +55,7 @@ def build_model(num_classes):
   ])
 
   # Compile the model to configure training parameters
-  model.compile(optimizer='adam',
+  model.compile(optimizer=tf.keras.optimizers.Adam(lr=lr),
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy'])
   return model
@@ -74,11 +80,9 @@ def main(_):
   images_count = len(all_image_paths)
   print(images_count)
 
-  split_1 = int(0.8 * images_count)
-  split_2 = int(0.9 * images_count)
-  train_images = all_image_paths[:split_1]
-  validate_images = all_image_paths[split_1:split_2]
-  test_images = all_image_paths[split_2:]
+  split = int(0.9 * images_count)
+  train_images = all_image_paths[:split]
+  validate_images = all_image_paths[split:]
 
   label_names = sorted(item.name for item in images_root.glob("*/") if item.is_dir())
   if FLAGS.debug_output:
@@ -90,7 +94,6 @@ def main(_):
 
   train_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in train_images]
   validate_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in validate_images]
-  test_image_labels = [label_to_index[pathlib.Path(path).parent.name] for path in test_images]
   if FLAGS.debug_output:
     print(train_image_labels)
 
@@ -130,23 +133,18 @@ def main(_):
   validate_label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(validate_image_labels, tf.int64))
   validate_image_label_ds = tf.data.Dataset.zip((validate_image_ds, validate_label_ds))
 
-  test_path_ds = tf.data.Dataset.from_tensor_slices(test_images)
-  test_image_ds = test_path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
-  test_label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(test_image_labels, tf.int64))
-  test_image_label_ds = tf.data.Dataset.zip((test_image_ds, test_label_ds))
-
-  inception_model = build_model(len(label_names))
-  num_train = split_1
-  num_val = split_2 - split_1
+  model = build_model(len(label_names))
+  num_train = len(train_images)
+  num_val = len(validate_images)
   steps_per_epoch = round(num_train) // BATCH_SIZE
   validation_steps = round(num_val) // BATCH_SIZE
 
   # Creating Keras callbacks
   tensorboard_callback = keras.callbacks.TensorBoard(log_dir='./log_dir', histogram_freq=1)
   model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        'training_checkpoints/weights.{epoch:02d}-{val_loss:.2f}.hdf5', period=5)
+        'training_checkpoints/weights.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', mode='min')
   os.makedirs('training_checkpoints/', exist_ok=True)
-  # early_stopping_checkpoint = keras.callbacks.EarlyStopping(patience=5)
+  early_stopping_checkpoint = keras.callbacks.EarlyStopping(monitor='val_accuracy', mode='max', min_delta=1)
 
   # Setting a shuffle buffer size as large as the dataset ensures that the data is
   # completely shuffled.
@@ -156,20 +154,16 @@ def main(_):
   # `prefetch` lets the dataset fetch batches, in the background while the model is training.
   ds = ds.prefetch(buffer_size=AUTOTUNE)
 
-  inception_model.fit(ds,
-                      epochs=FLAGS.epochs,
-                      steps_per_epoch=steps_per_epoch,
-                      validation_data=validate_image_label_ds.repeat().batch(BATCH_SIZE),
-                      validation_steps=validation_steps,
-                      callbacks=[tensorboard_callback,
-                                 model_checkpoint_callback])
-
-  predictions = inception_model.predict(test_image_label_ds.batch(BATCH_SIZE))
-  print(predictions)
+  model.fit(ds,
+            epochs=FLAGS.epochs,
+            steps_per_epoch=steps_per_epoch,
+            validation_data=validate_image_label_ds.repeat().batch(BATCH_SIZE),
+            validation_steps=validation_steps,
+            callbacks=[tensorboard_callback, model_checkpoint_callback, early_stopping_checkpoint])
 
   # Export the model to a SavedModel
   model_path = "{}/{}/".format(FLAGS.saved_model_dir, FLAGS.model_version)
-  keras.experimental.export_saved_model(inception_model, model_path)
+  keras.experimental.export_saved_model(model, model_path)
 
 
 if __name__ == '__main__':
@@ -195,12 +189,12 @@ if __name__ == '__main__':
   parser.add_argument(
       '--saved_model_dir',
       type=str,
-      default='./models/inception',
+      default='./models/mobilenet',
       help='Where to save the exported graph.')
   parser.add_argument(
     '--model_version',
     type=int,
-    default=3,
+    default=1,
     help="""Version number of the model.""")
 
   FLAGS, unparsed = parser.parse_known_args()
